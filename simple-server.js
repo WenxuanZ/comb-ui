7465b553b2080aeab3a814ced962f5910e914ea9
diozhu <diozhu@gmail.com>
2019-05-14 10:04:40 +0800

    Mod

--- simple-server.js
+++ simple-server.js
@@ -17,6 +17,14 @@ var PROJECT_NAME = process.env["PROJECT_NAME"] || 'comb';
 
 app.use(compression()); // gzip，内网联调可以打开。 mod by Dio Zhu. on 2018.11.14
 
+app.all("*", function(req,res,next) { //设置允许跨域的域名，*代表允许任意域名跨域 
+    res.header("Access-Control-Allow-Origin","*"); //允许的header类型 
+    res.header("Access-Control-Allow-Headers","content-type"); //跨域允许的请求方式 
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS"); 
+    if (req.method.toLowerCase() == 'options') res.send(200); //让options尝试请求快速结束 
+    else next(); 
+});
+
 app.use(function (req, res, next) {
     console.log('%s %s %s', req.method, req.url, req.path);
     next();
